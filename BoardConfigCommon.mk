5b0b5f073260ce15f456e5c1a4c0b32b175ab67e
Rashed Abdel-Tawab <rashed@linux.com>
2017-07-31 21:15:11 -0400

    g6: Add extra device HDR function
    Change-Id: I9fe414fa3e4afdda34375d0ddcae3acedc1dc923

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -19,6 +19,9 @@ DEVICE_COMMON_PATH := device/lge/g6-common
 # inherit from common msm8996
 -include device/lge/msm8996-common/BoardConfigCommon.mk
 
+# Display
+TARGET_PROVIDES_EXTRA_HDR_FUNCTION := true
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
