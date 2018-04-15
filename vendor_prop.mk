715a24836d1b7881dcbd50035a7e158badc7fbd4
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-15 09:59:05 -0700

    lucye: Move device properties to vendor
    Change-Id: I3b3d23f71ffd6edb35f4cf915d50d9dda9531f3d

---
+++ vendor_prop.mk
@@ -0,0 +1,7 @@
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.sf.color_saturation=1.1
+
+# HDR
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qcom.hdr.config=/system/vendor/etc/hdr_tm_config.xml
